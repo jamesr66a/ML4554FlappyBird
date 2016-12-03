8ac1832b47143b7af2e62f259815bb15dd693845
James Reed <jamesr66@vt.edu>
2016-12-02 23:28:37 -0500

    Replay memory test code and bugfix

--- ReplayMemory.py
+++ ReplayMemory.py
@@ -19,6 +19,18 @@ class ReplayMemory:
     if len(self.memory) < n:
       return None
 
-    idxs = np.random.choice(xrange(N), n, replace=False)
+    idxs = np.random.choice(xrange(self.N), n, replace=False)
     return [self.memory[idx] for idx in idxs]
 
+if __name__=='__main__':
+  rp = ReplayMemory(100)
+
+  for i in xrange(100):
+    rp.insert(
+      np.random.randn(4, 28, 28, 3),
+      np.random.randint(2),
+      np.random.randint(100),
+      np.random.randn(4, 28, 28, 3)
+    )
+
+  print(rp.sample(10))
